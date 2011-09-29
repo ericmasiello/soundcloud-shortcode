ef9d4891077c5126cb68b5b218b46cbd8e94769c
jowagener <jowagener@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-29 23:53:19 +0000

    fix linebreak issue
    git-svn-id: https://plugins.svn.wordpress.org/soundcloud-shortcode/trunk@445498 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- soundcloud-shortcode.php
+++ soundcloud-shortcode.php
@@ -3,7 +3,7 @@
 Plugin Name: SoundCloud Shortcode
 Plugin URI: http://www.soundcloud.com
 Description: SoundCloud Shortcode. Usage in your posts: [soundcloud]http://soundcloud.com/TRACK_PERMALINK[/soundcloud] . Works also with set or group instead of track. You can provide optional parameters height/width/params as follows [soundcloud height="82" params="auto_play=true"]http....
-Version: 1.2.1
+Version: 1.2.2
 Author: Johannes Wagener <johannes@soundcloud.com>
 Author URI: http://johannes.wagener.cc
 */
@@ -91,8 +91,8 @@ function soundcloud_shortcode( $atts, $url='' ) {
               return "<object height=\"" . esc_attr( $height ) . "\" width=\"" .
                      esc_attr( $width ) . "\"><param name=\"movie\" value=\"http://" .
                      esc_attr( $player_host ) . "/player.swf?" . esc_attr( $player_params ) .
-                     "\"></param><param name=\"allowscriptaccess\"
-                     value=\"always\"></param><embed allowscriptaccess=\"always\" height=\"" .
+                     "\"></param><param name=\"allowscriptaccess\" value=\"always\"></param>" . 
+                     "<embed allowscriptaccess=\"always\" height=\"" .
                      esc_attr( $height ) . "\" src=\"http://" . esc_attr( $player_host ) .
                      "/player.swf?" . esc_attr( $player_params ) .
                      "\" type=\"application/x-shockwave-flash\" width=\"" .
